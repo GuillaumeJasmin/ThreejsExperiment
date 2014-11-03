07e652d325b4d116765482f38d46469995daab8a
Guillaume Jasmin <guillaume.jasmin@gmail.com>
2014-11-04 00:09:59 +0100

    server webrtc

--- server-webrtc/index.js
+++ server-webrtc/index.js
@@ -1,7 +1,29 @@
 var PeerServer = require('peer').PeerServer;
+var express = require('express');
 
 var server = new PeerServer({port: 9000, path: '/'});
+var connectedUser = [];
 
-server.on('connection', function(id) {
-    console.log('id', id);
+// add user
+server.on('connection', function (id, a) {
+	connectedUser.push(id);
+	console.log('add', connectedUser);
 });
+
+// remove user
+server.on('disconnect', function (id) {
+	connectedUser.splice(connectedUser.indexOf(id), 1);
+	console.log('delete', connectedUser);
+});
+
+
+
+
+var app = express();
+
+app.get('/getconnected', function(req, res){
+	res.header('Access-Control-Allow-Origin', 'http://localhost:8000');
+	res.send(connectedUser);
+});
+
+app.listen(9001);
