3fcf2ab3cb4def4450e27d3472c1972bc77cf70a
Guillaume Jasmin <guillaume.jasmin@gmail.com>
2014-11-09 01:06:56 +0100

    log connection server

--- server-webrtc/index.js
+++ server-webrtc/index.js
@@ -1,19 +1,21 @@
 var PeerServer = require('peer').PeerServer;
 var express = require('express');
 
-var server = new PeerServer({host: 'threejs-experiment.guillaume-jasmin.fr', port: 9000, path: '/'});
+var server = new PeerServer({port: 9000, path: '/'});
+console.log('Starting Peer server on port 9000 ...');
+
 var connectedUser = [];
 
 // add user
 server.on('connection', function (id, a) {
 	connectedUser.push(id);
-	// console.log('add', connectedUser);
+	console.log('add', connectedUser);
 });
 
 // remove user
 server.on('disconnect', function (id) {
 	connectedUser.splice(connectedUser.indexOf(id), 1);
-	// console.log('delete', connectedUser);
+	console.log('delete', connectedUser);
 });
 
 
@@ -21,7 +23,9 @@ var app = express();
 
 app.get('/getconnected', function(req, res){
 	res.header('Access-Control-Allow-Origin', 'http://threejs-experiment.guillaume-jasmin.fr');
+    res.header('Access-Control-Allow-Origin', 'http://localhost:8000');
 	res.send(connectedUser);
 });
 
 app.listen(9001);
+console.log('Starting Express server on port 9001 ...');
